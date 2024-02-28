ddffa646448488cad6b454d502263cb67dde29f5
Ali Marzak <ali.marzak@live.fr>
2024-02-28 17:06:17 +0100

    Mise a jour

--- AlgoPHP_Partie1/exo15.php
+++ AlgoPHP_Partie1/exo15.php
@@ -3,12 +3,30 @@
 <?php
 
 class Personne {
-    var $nom;
-    var $prenom;
-    var $date_naissance;
+    private $nom;
+    private $prenom;
+    private $date_naissance;
+
+    public function getName()
+    {
+        //return $this->nom;
+        //return $this->prenom;
+        echo "$this->prenom $this->nom a $this->date_naissance";
+    }
+
+    public function __construct(string $nom, string $prenom, string $date_naissance)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->date_naissance = $date_naissance;
+    }
+
 }
 
 $p1 = new Personne("DUPONT", "Michel", "1980-02-19");
 $p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17");
 
+
+
+echo $p1->getName();
 
